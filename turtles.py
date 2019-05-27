f179c6b3f5ce18a637d4a0e9e6b831e438970918
Kim Kopowski <kim.kopowski@gmail.com>
2019-05-27 08:28:46 -0700

    MAINT - add __name__ - refactor load, clean and new features in separate functions - add requirments, too-versions, ingest more data

--- turtles.py
+++ turtles.py
@@ -5,46 +5,23 @@ import helpers as hlp
 
 
 def get_clean_data():
-    fileName1 = 'source/Turtle Data.xls'
-    fileName2 = 'source/MF Trapping Data.xlsx'
-    df = clean_data(fileName1, True)
-    df['Capture Location'] = 'Gresham'
+    fileName1 = 'Turtle Data.xls'
+    df1 = load_data('source/' + fileName1, True)
+    df1['Capture Location'] = 'Gresham'
 
-    df2 = clean_data(fileName2)
+    fileName2 = 'MF Trapping Data.xlsx'
+    df2 = load_data('source/' + fileName2)
     df2['Capture Location'] = 'Mason Flats'
-    df2['Source'] = 'MF Trapping Data.xls|All Capture Data'
-    cleaned = df.append(df2, sort=False)
 
-    # add features
-    cleaned['Age_To_Weight'] = cleaned['Annuli'] / cleaned['Weight']
-    buckets = 5
-    buckets = int(cleaned['Annuli'].max() / buckets)
-    labels = ["{0} - {1}".format(i, i + buckets)
-              for i in range(0, cleaned['Annuli'].max(), buckets)]
-    cleaned['Annuli_Group'] = pd.cut(cleaned.Annuli, range(
-        0, cleaned.Annuli.max() + buckets, buckets), right=False, labels=labels)
-    # Calcuate Number of recaptures
-    df = cleaned[['ID', 'Date']].groupby('ID').count()
-    df.columns = ['recapture_count']
-    df.reset_index(inplace=True)
-    cleaned = pd.merge(cleaned, df, how='outer', on='ID')
+    fileName3 = 'Whitaker Trapping Data.xlsx'
+    df3 = load_data('source/' + fileName3)
+    df3['Capture Location'] = 'Whitaker Ponds'
 
-    # recalculate annuli
-    df = pd.pivot_table(
-        cleaned[cleaned.Annuli > 0],
-        values=['Date', 'Annuli'],
-        index=['ID'],
-        aggfunc={'Date': min, 'Annuli': min})
-    df.columns = ['lowest_annuli', 'first_date']
-    df.reset_index(inplace=True)
-
-    cleaned = pd.merge(cleaned, df, how='outer', on='ID')
-    cleaned['date_year'] = cleaned.Date.map(lambda x: x.year)
-    cleaned['first_date_year'] = cleaned.first_date.map(lambda x: x.year)
-    cleaned['new_annuli'] = cleaned.date_year - \
-        cleaned.first_date_year + cleaned.lowest_annuli
-    cleaned.new_annuli = np.nan_to_num(cleaned.new_annuli)
-    return cleaned
+    print("Concat Loaded data...")
+    df = pd.concat([df1, df2, df3], sort=False)
+    df = clean_data(df)
+    df = new_features(df)
+    return df
 
 
 def load_data(fileName, big_file=False):
@@ -61,43 +38,93 @@ def load_data(fileName, big_file=False):
             df = df.append(new, sort=False)
     else:
         df = pd.read_excel(fileName)
+        df['Source'] = fileName
     return df
 
 
-def clean_data(fileName, big_file=False):
-    df = load_data(fileName, big_file)
+def clean_data(df):
+    ''' clean data (convert string to decimals, recode categorical data etc etc)
+    Original attribute values are maintained in a copy. Eg:
+        Weight: cleaned data
+        Weight_orig: original data
+    '''
 
-    # DATA CLEANING
-    cleaned = df.copy()
     # decimals
     print("Cleaning decimals ...")
-    cleaned['orig_Weight'] = cleaned.Weight
-    cleaned['Weight'] = cleaned['Weight'].apply(hlp.recode_decimal)
-    cleaned['Weight'] = pd.to_numeric(cleaned['Weight'], downcast='float')
-    cleaned['orig_Carapace'] = cleaned.Carapace
-    cleaned['Carapace'] = cleaned['Carapace'].apply(hlp.recode_decimal)
-    cleaned['Carapace'] = pd.to_numeric(cleaned['Carapace'], downcast='float')
-    cleaned['orig_Plastron'] = cleaned.Plastron
-    cleaned['Plastron'] = cleaned['Plastron'].apply(hlp.recode_decimal)
-    cleaned['Plastron'] = pd.to_numeric(cleaned['Plastron'], downcast='float')
-    cleaned['orig_Annuli'] = cleaned.Annuli
-    cleaned['Annuli'] = cleaned['Annuli'].apply(hlp.recode_decimal)
-    cleaned['Annuli'] = pd.to_numeric(cleaned['Annuli'], downcast='integer')
+    df['Weight_orig'] = df.Weight
+    df['Weight'] = df['Weight'].apply(hlp.recode_decimal)
+    df['Weight'] = pd.to_numeric(df['Weight'], downcast='float')
+    df['Carapace_orig'] = df.Carapace
+    df['Carapace'] = df['Carapace'].apply(hlp.recode_decimal)
+    df['Carapace'] = pd.to_numeric(df['Carapace'], downcast='float')
+    df['Plastron_orig'] = df.Plastron
+    df['Plastron'] = df['Plastron'].apply(hlp.recode_decimal)
+    df['Plastron'] = pd.to_numeric(df['Plastron'], downcast='float')
+    df['Annuli_orig'] = df.Annuli
+    df['Annuli'] = df['Annuli'].apply(hlp.recode_decimal)
+    df['Annuli'] = pd.to_numeric(df['Annuli'], downcast='integer')
 
     # other
     print("Cleaning other values ...")
-    cleaned['orig_Gender'] = cleaned.Gender
-    cleaned['Gender'] = cleaned['Gender'].apply(hlp.recode_sex)
-    cleaned['orig_Species'] = cleaned.Species
-    cleaned['Species'] = cleaned['Species'].apply(hlp.recode_species)
-    cleaned['orig_Gravid'] = cleaned.Gravid
-    cleaned['Gravid'] = cleaned['Gravid'].apply(hlp.recode_gravid)
+    df['Gender_orig'] = df.Gender
+    df['Gender'] = df['Gender'].apply(hlp.recode_sex)
+    df['Species_orig'] = df.Species
+    df['Species'] = df['Species'].apply(hlp.recode_species)
+    df['Gravid_orig'] = df.Gravid
+    df['Gravid'] = df['Gravid'].apply(hlp.recode_gravid)
+    return df
 
+
+def new_features(df):
+    ''' Add new features to data '''
+    print("Add new features ...")
     # distinguish Spring, Fall and pregnant females (don't care about juvenilles/unknown)
-    cleaned['gender_plus'] = cleaned['Gender']
-    cleaned.loc[cleaned.Gravid, 'gender_plus'] = 'f_gra'
+    df['gender_plus'] = df['Gender']
+    df.loc[df.Gravid, 'gender_plus'] = 'f_gra'
 
-    cleaned['gender_seasons'] = cleaned['Gender']
-    # cleaned.loc[cleaned.gender_seasons != 'unknown', 'gender_seasons'] = cleaned.Gender + '_' + cleaned.Date.apply(hlp.recode_season)
-    cleaned.loc[cleaned.Gravid, 'gender_seasons'] = 'f_gra'
-    return cleaned
+    df['gender_seasons'] = df['Gender']
+    df.loc[df.Gravid, 'gender_seasons'] = 'f_gra'
+
+    # add features
+    df['Age_To_Weight'] = df['Annuli'] / df['Weight']
+
+    # Calcuate Number of recaptures
+    df_captures = df[['ID', 'Date']].groupby('ID').count()
+    df_captures.columns = ['recapture_count']
+    df_captures.reset_index(inplace=True)
+    df = pd.merge(df, df_captures, how='outer', on='ID')
+
+    # recalculate annuli
+    df_min = pd.pivot_table(
+        df[df.Annuli > 0],
+        values=['Date', 'Annuli'],
+        index=['ID'],
+        aggfunc={'Date': min, 'Annuli': min})
+    df_min.columns = ['annuli_min', 'date_min']
+    df_min.reset_index(inplace=True)
+
+    df = pd.merge(df, df_min, how='outer', on='ID')
+    df['year'] = df.Date.map(lambda x: x.year)
+    df['year_min'] = df.date_min.map(lambda x: x.year)
+    df['Annuli_orig'] = df.Annuli
+    df.Annuli = df.year - \
+        df.year_min + df.annuli_min
+    df.Annuli = np.nan_to_num(df.Annuli)
+    df['Annuli'] = pd.to_numeric(df['Annuli'], downcast='integer')
+
+    # Annuli Buckets
+    buckets = 5
+    interval = int(df['Annuli'].max() / buckets)
+    buckets = [i for i in range(0, df['Annuli'].max() + interval, interval)]
+    labels = ["'{0} - {1}'".format(i, i + interval)
+              for i in buckets]
+    df['Annuli_Group'] = pd.cut(df.Annuli, buckets, labels=labels[:-1], include_lowest=True)
+
+    return df
+
+
+if __name__ == "__main__":
+    print("Loading, cleaning and transforming data...")
+    df = get_clean_data()
+    df.to_csv("turtles.csv")
+    print("Exported to turtles.csv")
